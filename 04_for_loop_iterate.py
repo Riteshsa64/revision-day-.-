b2042bbb0ea17f51ac480beb77d7c1be4015ddf2
Riteshsa64 <saroderitesh807@gmail.com>
2026-03-14 00:03:23 +0530

    Add files via upload

---
+++ 04_for_loop_iterate.py
@@ -0,0 +1,14 @@
+## For Loop with Lists
+l=[1,25,2,52,5,2]
+for i in l :
+    print(i)
+
+##For Loop with  Tuples
+t=(2,5,5,18575)
+for i in t :
+    print(i)
+
+##For Loop with strings
+s="Ritesh"
+for i in s:
+    print(i) 
